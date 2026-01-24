7d450a119a371bf1d6705f85500021ab0c7c0fa4
Anvesh-999 <anvesh.a9100@gmail.com>
2026-01-24 15:11:45 +0530

    pricing and education components

--- frontend/src/landing-page/home/Pricing.js
+++ frontend/src/landing-page/home/Pricing.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Pricing() {
     return ( 
-        <div className='container'>
+        <div className='container mt-5 pb-5 '>
             <div className='row'>
                 <div className='col-4'>
                     <h2 className='mb-3'>Unbeatable pricing</h2>
@@ -12,12 +12,12 @@ function Pricing() {
                 <div className='col-2'></div>
                 <div className='col-6'>
                     <div className='row text-center'>
-                        <div className='col p-2 border'>
-                            <h1><i class="fa-solid fa-indian-rupee-sign fs-3"></i>0</h1>
+                        <div className='col p-3 border'>
+                            <h1 className='mb-3'><i class="fa-solid fa-indian-rupee-sign fs-3"></i>0</h1>
                             <p>Free equity delivery and <br></br>direct mutual funds</p>
                         </div>
-                        <div className='col p-2 border'>
-                            <h1><i class="fa-solid fa-indian-rupee-sign fs-3"></i>20</h1>
+                        <div className='col p-3 border'>
+                            <h1 className='mb-3'><i class="fa-solid fa-indian-rupee-sign fs-3"></i>20</h1>
                             <p>Intraday and F&O</p>
                         </div>
                     </div>
