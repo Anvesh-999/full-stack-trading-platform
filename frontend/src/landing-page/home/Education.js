9cbdd0ca4f08995483d4cee3f49d0235db2b26c4
Anvesh-999 <anvesh.a9100@gmail.com>
2026-01-24 23:48:55 +0530

    education component

--- frontend/src/landing-page/home/Education.js
+++ frontend/src/landing-page/home/Education.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function Education() {
-    return ( 
-         <div className='container mt-5'>
-            <div className='row'>
-                <div className='col-6'>
-                    <img src="media/images/education.svg" style={{ width: "70%" }} />
-                </div>
-                <div className="col-6">
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-6">
+          <img src="media/images/education.svg" style={{ width: "70%" }} />
+        </div>
+        <div className="col-6">
           <h2 className="mb-3 fs-2">Free and open market education</h2>
           <p>
             Varsity, the largest online stock market education book in the world
@@ -24,9 +24,9 @@ function Education() {
             TradingQ&A <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
           </a>
         </div>
-            </div>
-        </div>
-     );
+      </div>
+    </div>
+  );
 }
 
 export default Education;
