561755e19b4b16749c23a7c597ac7c1cf2f5ff49
Anvesh-999 <anvesh.a9100@gmail.com>
2026-01-30 23:29:26 +0530

    landing page components

---
+++ frontend/src/landing-page/support/CreateTicket.js
@@ -0,0 +1,151 @@
+import React from "react";
+
+function CreateTicket() {
+  return (
+    <div className="container">
+      <div className="row p-5 mt-5">
+        <h1 className="fs-2">To create a ticket, select a relevant topic</h1>
+        <div className="col-4 p-5 mt-2">
+          <h4 className="">
+            <i class="fa fa-plus-circle" aria-hidden="true"></i> Account Opening
+          </h4>
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Company, Partnership and HUF Account
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Opening
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            NRI Account Opening
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Charges at Zerodha
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Zerodha IDFC FIRST Bank 3-in-1 
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Getting Started
+          </a>
+          <br />
+        </div>
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h4 className="">
+            <i class="fa fa-line-chart me-2" aria-hidden="true"></i> Trading & Orders
+          </h4>
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Placing Orders
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Order Types
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Margins
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Product Types
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Trade Execution
+          </a>
+          <br />
+        </div>
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h4 className="">
+            <i class="fa fa-money me-2" aria-hidden="true"></i> Funds
+          </h4>
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Add Funds
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Withdraw Funds
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Fund Transfer Issues
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Bank Accounts
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            UPI / Netbanking
+          </a>
+        </div>
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h4 className="">
+            <i class="fa fa-dashboard me-2" aria-hidden="true"></i> Console
+          </h4>
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Profile
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Reports
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Tax Statements
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Account Settings
+          </a>
+        </div>
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h4 className="">
+            <i class="fa fa-desktop me-2" aria-hidden="true"></i> Kite
+          </h4>
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Login Issues
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Charts
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Watchlists
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Order Errors
+          </a>
+        </div>
+        <div className="col-4 p-5 mt-2">
+          <h4 className="">
+            <i class="fa fa-file-text me-2" aria-hidden="true"></i> Charges & Taxes
+          </h4>
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Brokerage
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Taxes
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            GST
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Contract Notes
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CreateTicket;
