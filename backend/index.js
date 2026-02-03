acc3956e7e6942e37afdd9f49bc7d36d20e16c31
Anvesh-999 <anvesh.a9100@gmail.com>
2026-02-03 20:05:33 +0530

    express setup with mongodb

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3002;
+const uri= process.env.MONGO_URL;
+
+const app = express();
+
+
+app.listen(PORT, () => {
+  console.log(`Backend server is running on http://localhost:${PORT}`);
+  mongoose.connect(uri);
+  console.log('Connected to MongoDB');
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Backend Server');
+});
